7d29cb7700f351c15baac6b88ad1ae8f722f80ad
maicol <mmoarias4@gmail.com>
2021-10-07 09:46:24 -0500

    algunas pruebas

--- src/test/data.test.js
+++ src/test/data.test.js
@@ -5,17 +5,74 @@ const mongoose = require('mongoose')
 
 const api = supertest(app)
 
-test('save user', async () => {
+let transporter_token;
+let provider_token;
+let offer;
+
+test('guardar un transportador', async () => {
     const response = await api.post('/auth/register')
     .send({
-        username:"camione", 
-        email:"mai@gmail", 
+        username:"driver", 
+        email:"driver@gmail", 
         password:"123", 
-        rol: "PROVIDER",
+        rol: "CONVEYOR",
         vehicle_type: "TRUCK", 
         loading_capacity: 1000,
         description: "Camionero de 80 años ",
         cc: 90000
+    })
+    expect(response.status).toBe(201)
+})
+
+test('guardar un proveedor', async () => {
+    const response = await api.post('/auth/register')
+    .send({
+        username:"proveedor", 
+        email:"prov@gmail", 
+        password:"123", 
+        rol: "PROVIDER",
+        nit: "678enjkwueiro",
+        company_name: "Argos",
+        company_address: "Cra 1 Florida",
+        description: "Empresa productora de cemento"
+    })
+    expect(response.status).toBe(201)
+})
+
+test('generar token de autenticacion proveedor', async() => {
+    const res = await api.post('/auth/login')
+        .send({email: "prov@gmail", password:"123", });
+    provider_token = res.body.token; // save the token!
+})
+
+test('generar token de autenticacion transportador', async() => {
+    const res = await api.post('/auth/login')
+        .send({email: "driver@gmail", password:"123", });
+    transporter_token = res.body.token; // save the token!
+})
+
+test('crear una 0ferta con credenciales', async () => {
+    const response = await api.post('/offer')
+    .set('x-access-token', provider_token)
+    .send({
+        username:"proveedor", 
+        email:"prov@gmail", 
+        password:"123", 
+        rol: "PROVIDER",
+        nit: "678enjkwueiro",
+        company_name: "Argos",
+        company_address: "Cra 1 Florida",
+        description: "Empresa productora de cemento"
+    })
+    expect(response.status).toBe(201)
+})
+
+test('Aceptar una oferta', async () => {
+    const response = await api.post('/offer/')
+    .set('x-access-token', provider_token)
+    .send({
+        username:"proveedor", 
+        email:"prov@gmail"
     })
     expect(response.status).toBe(201)
 })
